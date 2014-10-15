38870920447439db7c380f0a535dd5c899505f9c
gdimaggio <gdimaggio91@gmail.com>
2014-10-15 15:43:18 +0100

    Adding logic to process the url responsible for the launch of the app

--- src/com/ziggeo/androidapp/MainActivity.java
+++ src/com/ziggeo/androidapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.ziggeo.androidapp;
 
+import java.util.List;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +21,20 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Check if the app was opened by an external app, i.e. the browser
+		if (getIntent().getData() != null) {
+			
+			// Get the url that triggered the app
+			Uri data = getIntent().getData();
+			
+			// Get its parameters, delimited by '/' 
+			List<String> params = data.getPathSegments();
+			
+			// Get the App token, whose position depends on the url type
+			String token = params.get(1); // "App token"
+
+		}
 
 	}
 
